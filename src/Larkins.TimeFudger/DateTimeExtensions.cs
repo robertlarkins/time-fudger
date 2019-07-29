0d93cbceb030c8f87ca15f5a77fcd4f18b8b1755
Robert Larkins <ayb4btu@gmail.com>
2019-07-30 08:46:07 +1200

    Add DateTimeExtensions.RoundToInterval
    This adds functionality to DateTime to round it to the interval
    specified. For example, if the interval is 5min, the returned
    DateTime will adjust to the nearest 5min.

---
+++ src/Larkins.TimeFudger/DateTimeExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Larkins.TimeFudger
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Rounds to interval. The interval starts at the midnight of the provided day.
+        /// </summary>
+        /// <param name="dateTime">The date time.</param>
+        /// <param name="interval">The interval.</param>
+        /// <returns></returns>
+        public static DateTime RoundToInterval(this DateTime dateTime, TimeSpan interval)
+        {
+            var intervalTicks = interval.Ticks;
+
+            if (intervalTicks == 0)
+            {
+                return dateTime;
+            }
+
+            var ticksSinceMidnight = dateTime.TimeOfDay.Ticks;
+            var ticksFromLastIntervalStep = ticksSinceMidnight % intervalTicks;
+
+            if (ticksFromLastIntervalStep == 0)
+            {
+                return dateTime;
+            }
+
+            var ticksFromMidnightToClosestInterval = intervalTicks * (int)Math.Round(ticksSinceMidnight / (double)intervalTicks);
+            var ticksToAdjustBy = ticksFromMidnightToClosestInterval - ticksSinceMidnight;
+
+            return dateTime.AddTicks(ticksToAdjustBy);
+        }
+    }
+}
---
+++ tests/Larkins.TimeFudger.Tests.Unit/DateTimeExtensionsTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Larkins.TimeFudger.Tests.Unit
+{
+    public class DateTimeExtensionsTests
+    {
+        [Fact]
+        public void RoundToInterval_AdjustToNearest5Min_DateTimeAdjustedUp()
+        {
+            // Arrange
+            var initialDateTime = new DateTime(2019, 7, 29, 13, 17, 30);
+            var timeSpan = new TimeSpan(0, 0, 5, 0);
+            var expected = new DateTime(2019, 7, 29, 13, 20, 0);
+
+            // Act
+            var result = initialDateTime.RoundToInterval(timeSpan);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void RoundToInterval_AdjustToNearest5Min_DateTimeAdjustedDown()
+        {
+            // Arrange
+            var initialDateTime = new DateTime(2019, 7, 29, 13, 17, 29);
+            var timeSpan = new TimeSpan(0, 0, 5, 0);
+            var expected = new DateTime(2019, 7, 29, 13, 15, 0);
+
+            // Act
+            var result = initialDateTime.RoundToInterval(timeSpan);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void RoundToInterval_NoInterval_NoChangeToDateTime()
+        {
+            // Arrange
+            var initialDateTime = new DateTime(2019, 7, 29, 13, 17, 32);
+            var timeSpan = new TimeSpan(0, 0, 0, 0);
+            var expected = new DateTime(2019, 7, 29, 13, 17, 32);
+
+            // Act
+            var result = initialDateTime.RoundToInterval(timeSpan);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void RoundToInterval_AdjustToNearest5Min_NoChangeToDateTime()
+        {
+            // Arrange
+            var initialDateTime = new DateTime(2019, 7, 29, 13, 20, 0);
+            var timeSpan = new TimeSpan(0, 0, 5, 0);
+            var expected = new DateTime(2019, 7, 29, 13, 20, 0);
+
+            // Act
+            var result = initialDateTime.RoundToInterval(timeSpan);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
