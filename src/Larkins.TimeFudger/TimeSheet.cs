004448b86010687c71c98b693d464b9b3cac0af1
Robert Larkins <ayb4btu@gmail.com>
2019-07-30 15:29:28 +1200

    Fix TimeSheet.TimeEntries
    TimeEntries would always return a new List, rather than being
    initialised with a new list.

--- src/Larkins.TimeFudger/TimeSheet.cs
+++ src/Larkins.TimeFudger/TimeSheet.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Larkins.TimeFudger
 {
+    /// <summary>
+    /// TODO: Make this implement IEnumerable or extend SortedList
+    /// </summary>
     public class TimeSheet
     {
-        public List<TimeEntry> TimeEntries => new List<TimeEntry>();
+        public List<TimeEntry> TimeEntries { get; } = new List<TimeEntry>();
     }
 }
